4ce7cfc79cf026429c20fc187b65745b587c5bdb
desh200 <umesh.deshmukh35@gmail.com>
2021-10-06 12:19:47 +0530

    2 files modified, 2 added

---
+++ src/app/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, VERSION } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'login',
+  templateUrl: 'login.component.html',
+})
+export class LoginComponent {
+  form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+}

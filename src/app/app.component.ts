b4f6a420f506f06ae823a886eabe38b20c4c1c58
desh200 <umesh.deshmukh35@gmail.com>
2021-11-17 09:27:01 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component, VERSION } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  searchcontrol = new FormControl();
+
+  constructor() {
+    this.searchcontrol.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
 }
